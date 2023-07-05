c7514fa1f97036833e63de6c9fd81ffad1792f8e
A-Kija <arvydas.kijakauskas@gmail.com>
2023-07-05 12:55:18 +0300

    inputs

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,18 +1,19 @@
 import './App.scss';
-import Sq from './Components/005/Sq';
-// import Counter from './Components/005/Counter';
+import Fancy from './Components/006/Fancy';
+import Input from './Components/006/Input';
+import Input2 from './Components/006/Input2';
 import './buttons.scss';
-// import Dogs4 from './Components/005/Dogs4';
-// import Dogs5 from './Components/005/Dogs5';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <Dogs4 />
-        <Dogs5 /> */}
-        {/* <Counter/> */}
-        <Sq />
+        
+        
+        <Fancy />
+        <Input2 />
+        <Input />
       </header>
     </div>
   );
