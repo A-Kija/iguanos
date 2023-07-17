c161ef07d63c87a6398d157ce01bc427e74fdb0f
A-Kija <arvydas.kijakauskas@gmail.com>
2023-07-17 14:35:49 +0300

    mas

--- 011/index.php
+++ 011/index.php
@@ -16,10 +16,55 @@ $spintele = [
 
 $spintele[20] = 'papuga';
 
-print_r($spintele);
+// array_push($spintele, 'varna');
+$spintele[] = 'varna';
+
+// array_unshift($spintele, 'strutis');
+
+// array_pop($spintele);
+// array_shift($spintele);
+
+
 
 echo '<br>';
 
+foreach ($spintele as $key => &$value) {}
+
+unset($value);
+
+foreach ($spintele as $key => $value) {}
+
+
+
+// for ($i = 1; $i <= 5; $i++) {
+//     echo $i;
+// }
+
+
+// echo '<br>';
+
+
+// foreach (range('a', 'k') as $value) {
+//     echo $value;
+// }
+ 
+
+echo '<br>';
+
+
+
+
+
+print_r($spintele);
+
+usort($spintele, function($a, $b) {
+    return  strlen($b) <=> strlen($a);
+});
+
+print_r($spintele);
+
+
+// print_r(range('a', 'k'));
 
 
 // echo $spintele[4];
