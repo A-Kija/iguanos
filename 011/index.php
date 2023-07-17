62ebcb6b10239780c52171cae153b27ee798b6aa
A-Kija <arvydas.kijakauskas@gmail.com>
2023-07-17 13:16:09 +0300

    pieva

---
+++ 011/index.php
@@ -0,0 +1,25 @@
+<?php
+echo '<pre>';
+
+$spintele = [
+    5 => 'bebras',
+    true => 'vilkas',
+    '' => 'lape',
+    'pieva' => 'zuikis',
+    'barsukas',
+    'vovere',
+    'suo',
+   -33 => 'kate',
+    'narvas' => 'peleda',
+    'sernas',
+];
+
+$spintele[20] = 'papuga';
+
+print_r($spintele);
+
+echo '<br>';
+
+
+
+// echo $spintele[4];
