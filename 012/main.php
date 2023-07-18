9306894493e8026ecb2092fb8e2b0648ffa5f94f
A-Kija <arvydas.kijakauskas@gmail.com>
2023-07-18 14:23:29 +0300

    const

--- 012/main.php
+++ 012/main.php
@@ -1,10 +1,11 @@
 <?php
+define('GATE', 'OK');
 
 echo '<h1>MAIN</h1>';
 
 
-require_once __DIR__ . '/inc.php';
-require_once __DIR__ . '/inc.php';
+require __DIR__ . '/inc.php';
+require __DIR__ . '/inc.php';
 
 
 echo '<h1>MAIN END</h1>';
