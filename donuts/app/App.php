0f6cb9a2eb044d97c9f1afaf70a3b6529fb04e25
A-Kija <arvydas.kijakauskas@gmail.com>
2023-08-08 13:16:00 +0300

    laravel mix

--- donuts/app/App.php
+++ donuts/app/App.php
@@ -1,10 +1,32 @@
 <?php
 namespace Donuts;
 
+use Donuts\Controllers\DonutsController as DC;
+
 class App {
 
     public static function start() {
-        echo '<h1>Hi from App</h1>';
+        return self::router();
+    }
+
+
+    public static function router()
+    {
+        $uri = $_SERVER['REQUEST_URI'];
+        $uri = explode('/', $uri);
+        array_shift($uri);
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        if ($method == 'GET' && count($uri) == 1 && $uri[0] == 'donuts') {
+            return (new DC)->index();
+        }
+
+
+
+
+        return '<h1> 404 Page not found </h1>';
+
+
     }
 
 }
--- donuts/public/index.php
+++ donuts/public/index.php
@@ -3,7 +3,5 @@ use Donuts\App;
 
 require_once '../vendor/autoload.php';
 
-echo '<h1>Hi from index.php</h1>';
 
-
-App::start();
+echo App::start();
